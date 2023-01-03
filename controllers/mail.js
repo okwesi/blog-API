15255c96a38580fe2b19482750d82816074579f5
okwesi <okwesi73@gmail.com>
2023-01-03 20:09:21 +0000

    security updates

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,12 +1,14 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config()
+
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
-    user: "gyamfiowusu630@gmail.com",
-    pass: "jujkzafsyudbnmdx", // generated ethereal password
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD, // generated ethereal password
   },
 });
 
