bf2f058ffeb4820b8e2b5e6609cca756ef3a9034
okwesi <okwesi73@gmail.com>
2023-01-14 21:15:21 +0000

    deleted signup test

--- test/userTest.js
+++ test/userTest.js
@@ -98,22 +98,4 @@ describe("Test User Sign In", () => {
   });
 });
 
-/////////////////////////////////////////////////////////////////////
-describe("Test User Sign Up", () => {
-  it("should return status code 201 and user Data", (done) => {
-    const options = {
-      url: "http://localhost:5000/users/signup/",
-      method: "POST",
-      json: {
-        name: "django test",
-        email: "djangotest4@gmail.com",
-        password: "123456",
-      },
-    };
-    request.get(options, (error, response, body) => {
-      assert.equal(201, response.statusCode);
-      assert.deepEqual("string", typeof JSON.parse(body.user_id));
-      done();
-    });
-  });
-});
+
